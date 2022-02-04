464bfe3b733e0bf4e306b193ae16069f34fe4cd7
“kukim” <kukim.dev@gmail.com>
2022-02-04 16:33:56 +0900

    green: Method 클래스의 메타 정보 활용해서 해당 메서드 이름 실행

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -1,9 +1,28 @@
 package xunit;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
     public boolean wasRun;
+    public String name;
+
+    public WasRun(String name) {
+        this.name = name;
+
+    }
 
     public void testMethod() {
         wasRun = true;
+    }
+
+    public void run() {
+        try {
+            Method method = getClass().getMethod(name);
+            method.invoke(this);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
