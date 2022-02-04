939a7922efe2591eda3451c54c17c2851f04a582
“kukim” <kukim.dev@gmail.com>
2022-02-04 20:04:44 +0900

    red: 매 테스트 간 독립 실행을 위해 setup 기능 추가 테스트 생성

--- src/xunit/xUnitTest.java
+++ src/xunit/xUnitTest.java
@@ -3,5 +3,6 @@ package xunit;
 public class xUnitTest {
     public static void main(String[] args) {
         new TestCaseTest("testRunning").run();
+        new TestCaseTest("testSetUp").run();
     }
 }
