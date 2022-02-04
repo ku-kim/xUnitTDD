50c2b30d0f01bc0c6cec5fa87dc75534b36acc2d
“kukim” <kukim.dev@gmail.com>
2022-02-04 16:31:10 +0900

    red: 테스트 메서드명 입력받아 사용

--- src/xunit/xUnitTest.java
+++ src/xunit/xUnitTest.java
@@ -2,9 +2,9 @@ package xunit;
 
 public class xUnitTest {
     public static void main(String[] args) {
-        WasRun wasRun = new WasRun();
+        WasRun wasRun = new WasRun("testMethod");
         System.out.println(wasRun.wasRun); // false
-        wasRun.testMethod();
+        wasRun.run();
         System.out.println(wasRun.wasRun); // true
     }
 }
