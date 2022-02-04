66aae3d66a32fd49b7f90884c101b75b6c9e8cc1
“kukim” <kukim.dev@gmail.com>
2022-02-04 20:56:14 +0900

    green:

--- src/xunit/TestResult.java
+++ src/xunit/TestResult.java
@@ -2,12 +2,17 @@ package xunit;
 
 public class TestResult {
     int runCount = 0;
+    int errorCount = 0;
 
     public void testStarted() {
         runCount++;
     }
-    
+
+    public void testFailed() {
+        errorCount++;
+    }
+
     public String getSummary() {
-        return runCount + " run, 0 failed";
+        return runCount + " run, " + errorCount + " failed";
     }
 }
